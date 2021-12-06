8176fd60f4ee184beeb5bec5c715dd052923e3a9
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2021-12-06 09:50:42 -0700

    Update plotting scripts

--- plotter.py
+++ plotter.py
@@ -50,6 +50,12 @@ if __name__ == "__main__":
     # Parse arguments
     parser = argparse.ArgumentParser(description="A simple plot tool")
     parser.add_argument("-f", "--fdir", nargs="+", help="Folder to plot", required=True)
+    parser.add_argument(
+        "--tmin",
+        help="Time at which to start statistics (tau units)",
+        default=0,
+        type=float,
+    )
     args = parser.parse_args()
 
     # Reference data
@@ -85,15 +91,18 @@ if __name__ == "__main__":
     cidx = cmap[-1]
     legend_elements += (Line2D([0], [0], lw=2, color=cidx, label="Johnson (1999)"),)
     df = pd.read_csv(os.path.join(jpdir, "cd.txt"))
-    plt.figure("cd")
     row = df[df.Re == Re]
-    p = plt.axhline(row.cd.item(), xmin=0, xmax=30, lw=2, color=cidx, ls="--")
-    plt.axhspan(
-        row.cd.item() - row.cd_err.item(),
-        row.cd.item() + row.cd_err.item(),
-        facecolor=cidx,
-        alpha=0.5,
-    )
+    for coef in ["cd", "cl"]:
+        plt.figure(coef)
+        p = plt.axhline(row[coef].item(), xmin=0, xmax=30, lw=2, color=cidx, ls="--")
+        plt.axhspan(
+            row[coef].item() - row[coef+"_err"].item(),
+            row[coef].item() + row[coef+"_err"].item(),
+            facecolor=cidx,
+            alpha=0.5,
+        )
+        plt.figure(coef+"-spectra")
+        p = plt.axvline(row.st.item(), ymin=0, ymax=30, lw=2, color=cidx, ls="--")
 
     cidx = cmap[-2]
     legend_elements += (Line2D([0], [0], lw=2, color=cidx, label="Tomboulides (2000)"),)
@@ -107,6 +116,8 @@ if __name__ == "__main__":
         facecolor=cidx,
         alpha=0.5,
     )
+    plt.figure("cd-spectra")
+    p = plt.axvline(row.st.item(), ymin=0, ymax=30, lw=2, color=cidx, ls="--")
 
     cidx = cmap[-3]
     legend_elements += (Line2D([0], [0], lw=2, color=cidx, label="Nagata (2020)"),)
@@ -127,7 +138,7 @@ if __name__ == "__main__":
     for i, fdir in enumerate(args.fdir):
 
         yname = os.path.join(fdir, "sphere-nwind.yaml")
-        u0, rho0, mu, turb_model = utilities.parse_ic(yname)
+        u0, rho0, mu, turb_model, dt = utilities.parse_ic(yname)
         dynPres = rho0 * 0.5 * u0 * u0
         diameter = 1.0
         refArea = np.pi * diameter * diameter / 4.0
@@ -142,23 +153,60 @@ if __name__ == "__main__":
             (pd.read_csv(f, delim_whitespace=True) for f in all_files)
         ).sort_values(by="Time", ignore_index=True)
         df["t"] = df.Time / tau
+        df = df[df["t"] > args.tmin]
         df["px"] = df.Fpx / (dynPres * refArea)
         df["vx"] = df.Fvx / (dynPres * refArea)
         df["cd"] = (df.Fpx + df.Fvx) / (dynPres * refArea)
-        df["cl"] = (df.Fpy + df.Fvy) / (dynPres * refArea)
-        df["cs"] = (df.Fpz + df.Fvz) / (dynPres * refArea)
+        df["cy"] = (df.Fpy + df.Fvy) / (dynPres * refArea)
+        df["cz"] = (df.Fpz + df.Fvz) / (dynPres * refArea)
+        alpha = utilities.find_symmetry_plane(df)
+        df["cl"] = -(np.cos(alpha) * df.cy - np.sin(alpha) * df.cz)
+        df["cs"] = np.sin(alpha) * df.cy + np.cos(alpha) * df.cz
 
-        plt.figure("cd")
-        p = plt.plot(df["t"], df.cd, lw=2, color=cmap[i])
+        means = df.mean()
+        stats = {}
+        for coef in ["cd", "cl", "cs"]:
+            stats[coef] = means[coef]
+            stats[coef +"_err"] = df[coef].max() - means[coef]
+
+            plt.figure(coef)
+            p = plt.plot(df["t"], df[coef], lw=2, color=cmap[i])
+            p[0].set_dashes(dashseq[i])
+
+            fhat, yhat = utilities.getFFT(dt, df[coef], normalize=True, window=False)
+            yhat = np.abs(yhat[1:])
+            fhat = fhat[1:]
+            stats[coef + "_st"] = fhat[np.argmax(yhat)]
+            plt.figure(coef+"-spectra")
+            p = plt.plot(fhat * tau, yhat, lw=2, color=cmap[i])
+            
+        wdf = pd.read_csv(os.path.join(fdir, "results", "wall.dat"))
+        wdf.phi = 180 - np.fabs(wdf.phi)
+        wdf.sort_values(by=["phi"], inplace=True)
+        wdf["cp"] = wdf.pressure / dynPres
+        wdf["cf"] = wdf.tauw / dynPres
+
+        bins = np.linspace(0, 180, 100)
+        group = wdf.groupby(pd.cut(wdf.phi, bins))
+        plot_centers = (bins[:-1] + bins[1:]) / 2
+        cp_values = group.cp.mean()
+        cf_values = group.cf.mean()
+
+        plt.figure("cp")
+        # p = plt.plot(wdf.phi, wdf.cp, lw=2, color=cmap[i], linestyle="None", marker="s", ms=1)
+        # p = plt.plot(theta, cp, lw=2, color=cmap[-1])
+        # p = plt.plot(phi, cp, lw=2, color=cmap[-1])
+        p = plt.plot(plot_centers, cp_values, lw=2, color=cmap[-1])
         p[0].set_dashes(dashseq[i])
 
-        # plt.figure("cl")
-        # p = plt.plot(df["t"], df.cl, lw=2, color=cmap[i])
-        # p[0].set_dashes(dashseq[i])
+        plt.figure("cf")
+        # p = plt.plot(wdf.theta, wdf.cf, lw=2, color=cmap[i])
+        p = plt.plot(plot_centers, cf_values, lw=2, color=cmap[-1])
+        p[0].set_dashes(dashseq[i])
 
-        # plt.figure("cs")
-        # p = plt.plot(df["t"], df.cs, lw=2, color=cmap[i])
-        # p[0].set_dashes(dashseq[i])
+        print(f"""Stats for {fdir}""")
+        print(f"""Symmetry plane is at alpha = {np.degrees(alpha[0])}""")
+        print(stats)
 
     # Save the plots
     with PdfPages(fname) as pdf:
@@ -171,6 +219,54 @@ if __name__ == "__main__":
         plt.setp(ax.get_ymajorticklabels(), fontsize=18, fontweight="bold")
         # plt.xlim([0, 30])
         plt.ylim([0.6, 0.7])
+        legend = ax.legend(handles=legend_elements, loc="best")
+        plt.tight_layout()
+        pdf.savefig(dpi=300)
+
+        plt.figure("cl")
+        ax = plt.gca()
+        plt.xlabel(r"$t$", fontsize=22, fontweight="bold")
+        plt.ylabel(r"$c_L$", fontsize=22, fontweight="bold")
+        plt.setp(ax.get_xmajorticklabels(), fontsize=18, fontweight="bold")
+        plt.setp(ax.get_ymajorticklabels(), fontsize=18, fontweight="bold")
+        # plt.xlim([0, 30])
+        plt.ylim([-0.1, 0])
+        legend = ax.legend(handles=legend_elements, loc="best")
+        plt.tight_layout()
+        pdf.savefig(dpi=300)
+
+        plt.figure("cs")
+        ax = plt.gca()
+        plt.xlabel(r"$t$", fontsize=22, fontweight="bold")
+        plt.ylabel(r"$c_S$", fontsize=22, fontweight="bold")
+        plt.setp(ax.get_xmajorticklabels(), fontsize=18, fontweight="bold")
+        plt.setp(ax.get_ymajorticklabels(), fontsize=18, fontweight="bold")
+        # plt.xlim([0, 30])
+        plt.ylim([-0.1, 0.1])
+        legend = ax.legend(handles=legend_elements, loc="best")
+        plt.tight_layout()
+        pdf.savefig(dpi=300)
+
+        plt.figure("cd-spectra")
+        ax = plt.gca()
+        plt.xlabel(r"$St$", fontsize=22, fontweight="bold")
+        plt.ylabel(r"$\hat{C}_D$", fontsize=22, fontweight="bold")
+        plt.setp(ax.get_xmajorticklabels(), fontsize=18, fontweight="bold")
+        plt.setp(ax.get_ymajorticklabels(), fontsize=18, fontweight="bold")
+        plt.xlim([0, 1])
+        plt.ylim([0, 2e-3])
+        legend = ax.legend(handles=legend_elements, loc="best")
+        plt.tight_layout()
+        pdf.savefig(dpi=300)
+
+        plt.figure("cl-spectra")
+        ax = plt.gca()
+        plt.xlabel(r"$St$", fontsize=22, fontweight="bold")
+        plt.ylabel(r"$\hat{C}_L$", fontsize=22, fontweight="bold")
+        plt.setp(ax.get_xmajorticklabels(), fontsize=18, fontweight="bold")
+        plt.setp(ax.get_ymajorticklabels(), fontsize=18, fontweight="bold")
+        plt.xlim([0, 1])
+        plt.ylim([0, 1e-2])
         legend = ax.legend(handles=legend_elements, loc="best")
         plt.tight_layout()
         pdf.savefig(dpi=300)
@@ -198,27 +294,3 @@ if __name__ == "__main__":
         legend = ax.legend(handles=legend_elements, loc="best")
         plt.tight_layout()
         pdf.savefig(dpi=300)
-
-        # plt.figure("cl")
-        # ax = plt.gca()
-        # plt.xlabel(r"$t$", fontsize=22, fontweight="bold")
-        # plt.ylabel(r"$c_L$", fontsize=22, fontweight="bold")
-        # plt.setp(ax.get_xmajorticklabels(), fontsize=18, fontweight="bold")
-        # plt.setp(ax.get_ymajorticklabels(), fontsize=18, fontweight="bold")
-        # # plt.xlim([0, 30])
-        # plt.ylim([-0.5, 0.5])
-        # legend = ax.legend(handles=legend_elements, loc="best")
-        # plt.tight_layout()
-        # pdf.savefig(dpi=300)
-        # plt.figure("cl")
-
-        # ax = plt.gca()
-        # plt.xlabel(r"$t$", fontsize=22, fontweight="bold")
-        # plt.ylabel(r"$c_S$", fontsize=22, fontweight="bold")
-        # plt.setp(ax.get_xmajorticklabels(), fontsize=18, fontweight="bold")
-        # plt.setp(ax.get_ymajorticklabels(), fontsize=18, fontweight="bold")
-        # # plt.xlim([0, 30])
-        # plt.ylim([-0.5, 0.5])
-        # legend = ax.legend(handles=legend_elements, loc="best")
-        # plt.tight_layout()
-        # pdf.savefig(dpi=300)
--- utilities.py
+++ utilities.py
@@ -1,8 +1,11 @@
+"""Utilities for scripts."""
 import yaml
+import numpy as np
+from scipy.optimize import fsolve
 
 
 def parse_ic(fname):
-    """Parse the Nalu yaml input file for the initial conditions"""
+    """Parse the Nalu yaml input file for the initial conditions."""
     with open(fname, "r") as stream:
         try:
             dat = yaml.full_load(stream)
@@ -16,8 +19,53 @@ def parse_ic(fname):
                 dat["realms"][0]["material_properties"]["specifications"][1]["value"]
             )
             turb_model = dat["realms"][0]["solution_options"]["turbulence_model"]
+            dt = dat["Time_Integrators"][0]["StandardTimeIntegrator"]["time_step"]
 
-            return u0, rho0, mu, turb_model
+            return u0, rho0, mu, turb_model, dt
 
         except yaml.YAMLError as exc:
             print(exc)
+
+
+def tukeyWindow(N, params={"alpha": 0.1}):
+    """Tukey window.
+
+    See https://en.wikipedia.org/wiki/Window_function#Tukey_window
+    """
+    alpha = params["alpha"]
+    w = np.zeros(N)
+    L = N + 1
+    for n in np.arange(0, int(N // 2) + 1):
+        if (0 <= n) and (n < 0.5 * alpha * L):
+            w[n] = 0.5 * (1.0 - np.cos(2 * np.pi * n / (alpha * L)))
+        elif (0.5 * alpha * L <= n) and (n <= N / 2):
+            w[n] = 1.0
+        else:
+            print("Something wrong happened at n = ", n)
+        if n != 0:
+            w[N - n] = w[n]
+    return w
+
+
+def getFFT(dt, y, normalize=False, window=True):
+    """Compute FFT."""
+    n = len(y)
+    k = np.arange(n)
+    frq = k / (n * dt)
+    window = tukeyWindow(n) if window else 1.0
+    norm = len(y) if normalize else 1.0
+    ffty = np.fft.fft(window * y) / norm
+
+    # Take the one sided version of it
+    freq = frq[range(int(n // 2))]
+    ffty = ffty[range(int(n // 2))]
+    return freq, ffty
+
+
+def find_symmetry_plane(df):
+    """Find symmetry plane.
+
+    Do this by finding the angle which sets the side force to zero."""
+
+    angle = fsolve(lambda x: np.mean(np.sin(x) * df.cy + np.cos(x) * df.cz), 0.0)
+    return angle
